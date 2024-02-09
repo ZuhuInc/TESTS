7c1cecaf8cc20cb0b279f99fe3be61b3203c1574
ZuhuInc <66175704+ZuhuInc@users.noreply.github.com>
2024-02-09 13:30:54 +0100

    Create OFFSCLIEN.py

---
+++ OFFSCLIEN.py
@@ -0,0 +1,8 @@
+    #offsets.py
+    dwEntityList = 0x18B0FC8
+    dwLocalPlayerPawn = 0x17262E8
+    #client.dll.py
+    m_iTeamNum = 0x3CB
+    m_iHealth = 0x334
+    m_vOldOrigin = 0x127C
+    m_hPlayerPawn = 0x7E4

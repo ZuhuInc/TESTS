a94c7aa6bc78f4a8ad4742c2438942c994aa15dd
ZuhuInc <66175704+ZuhuInc@users.noreply.github.com>
2024-05-25 18:32:55 +0200

    Offsets Update 25/5/24

--- OFFSCLIEN.py
+++ OFFSCLIEN.py
@@ -1,11 +1,11 @@
 #offsets.py
-dwEntityList = 0x18C7F98
-dwLocalPlayerPawn = 0x173B568
+dwEntityList = 0x19A3328
+dwLocalPlayerPawn = 0x180DB18
 #client.dll.py
-m_iTeamNum = 0x3CB
-m_iHealth = 0x334
-m_vOldOrigin = 0x127C
-m_hPlayerPawn = 0x7E4
+m_iTeamNum = 0x3C3
+m_iHealth = 0x324
+m_vOldOrigin = 0x1274
+m_hPlayerPawn = 0x7DC
 m_iIDEntIndex = 0x13B0
 
 

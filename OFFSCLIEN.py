829dfaf23a2061148787aaa44ebe9b02f5efb1e4
ZuhuInc <66175704+ZuhuInc@users.noreply.github.com>
2024-06-07 12:42:15 +0200

    Update test offsets error wrong value

--- OFFSCLIEN.py
+++ OFFSCLIEN.py
@@ -1,11 +1,11 @@
 #offsets.py
-dwEntityList = 0x19A3328
-dwLocalPlayerPawn = 0x180DB18
+dwEntityList = 0x195C460
+dwLocalPlayerPawn = 0x17C9468
 #client.dll.py
 m_iTeamNum = 0x3C3
 m_iHealth = 0x324
 m_vOldOrigin = 0x1274
 m_hPlayerPawn = 0x7DC
-m_iIDEntIndex = 0x13B0
+m_iIDEntIndex = 0x13A8
 
 

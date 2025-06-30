12e6576f2787687386d5cd4ba638e2ae2cf9b4da
ZuhuInc <66175704+ZuhuInc@users.noreply.github.com>
2025-06-30 20:48:07 +0200

    Delete OFFSCLIEN.py
    UNUSED

--- OFFSCLIEN.py
+++
@@ -1,9 +0,0 @@
-#offsets.py
-dwEntityList = 0x1A38800
-dwLocalPlayerPawn = 0x188CF70
-#client.dll.py
-m_iTeamNum = 0x3E3
-m_iHealth = 0x344
-m_vOldOrigin = 0x1324
-m_hPlayerPawn = 0x814
-m_iIDEntIndex = 0x1458
